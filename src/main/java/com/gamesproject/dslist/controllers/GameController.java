fda603ed989c544ed6fbd6a4cb35be4f5dc94fed
vhraposo <thevhup@gmail.com>
2023-05-10 03:48:40 -0300

    Add Game Repository, service, controller

---
+++ src/main/java/com/gamesproject/dslist/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.gamesproject.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gamesproject.dslist.dto.GameMinDTO;
+import com.gamesproject.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll(){
+		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}
+}
---
+++ src/main/java/com/gamesproject/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.gamesproject.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gamesproject.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+	
+}
