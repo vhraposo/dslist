c1f59a5bb724be7f6e00af9e14f4696f1775f29e
vhraposo <thevhup@gmail.com>
2023-05-13 08:28:02 -0300

    Move games on list

--- src/main/java/com/gamesproject/dslist/repositories/GameListRepository.java
+++ src/main/java/com/gamesproject/dslist/repositories/GameListRepository.java
@@ -1,22 +1,15 @@
 package com.gamesproject.dslist.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.gamesproject.dslist.entities.GameList;
-import com.gamesproject.dslist.projections.GameMinProjection;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
 	
-	@Query(nativeQuery = true, value = """
-			SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
-			tb_game.short_description AS shortDescription, tb_belonging.position
-			FROM tb_game
-			INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
-			WHERE tb_belonging.list_id = :listId
-			ORDER BY tb_belonging.position
-				""")
-	List<GameMinProjection> searchByList(Long listId);
+	@Modifying
+	@Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
+	
 }
--- src/main/java/com/gamesproject/dslist/services/GameListService.java
+++ src/main/java/com/gamesproject/dslist/services/GameListService.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.gamesproject.dslist.dto.GameDTO;
 import com.gamesproject.dslist.dto.GameListDTO;
-import com.gamesproject.dslist.dto.GameMinDTO;
-import com.gamesproject.dslist.entities.Game;
 import com.gamesproject.dslist.entities.GameList;
+import com.gamesproject.dslist.projections.GameMinProjection;
 import com.gamesproject.dslist.repositories.GameListRepository;
 import com.gamesproject.dslist.repositories.GameRepository;
 
@@ -20,10 +18,26 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository; 
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 		
+	}
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for ( int i = min; i<= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
